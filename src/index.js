3e76d0feff920f5895325b2c6dfc1e5e13699c1e
Joe Bumbulis <joebumbulis@gmail.com>
2024-05-03 11:02:17 -0500

    refactors isGet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 var src_default = {
 	async fetch(request) {
+		const isGetRequest = (request) => request.method === "GET";
 		const max = 4; 
 		const tableCodes = {
 			0: "1",
@@ -9,13 +10,12 @@ var src_default = {
 			3: "4"
 		};
 
-		const generateRandomNumber = function (tableCodes, max) {
+		const generateRandomNumber = (tableCodes, max) => {
 			const randomNumber = Math.floor(Math.random() * max);
-
 			return tableCodes[randomNumber];
 		};
 
-		const response = request.method === 'GET' ? `Random Number Generated: ${generateRandomNumber(tableCodes, max)}` : "please use GET";
+		const response = isGetRequest ? `Random Number Generated: ${generateRandomNumber(tableCodes, max)}` : "please use GET";
 
 		return new Response(response);
 	}
