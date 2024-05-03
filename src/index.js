6e2806ecc06f4e232e460053780e5b28c43d2e5f
Joe Bumbulis <joebumbulis@gmail.com>
2024-05-03 10:41:12 -0500

    cleans up response out of function

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 // src/index.js
 var src_default = {
 	async fetch(request) {
+		const tableCodes = {
+			0: "1",
+			1: "2",
+			2: "3",
+			3: "4"
+		};
+
 		if (request.method === "GET") {
-			const tableCodes = {
-				0: "1",
-				1: "2",
-				2: "3",
-				3: "4"
-			};
-
-			function generateRandomNumber(response, tableCodes) {
+			let generateRandomNumber = function (request, tableCodes) {
 				const randomNumber = Math.floor(Math.random() * 4);
-				return new Response(`This is a random number between 1 - 4: ${tableCodes[randomNumber]}`);
-			}
 
-			return generateRandomNumber(request, tableCodes);
+				return tableCodes[randomNumber];
+			};
+			return new Response(`Random Number Generated: ${generateRandomNumber(request, tableCodes)}`);
+
 		} else {
-			return new Response('please use GET')
+			return new Response("please use GET");
 		}
 	}
 };
-
 export {
 	src_default as default
 };
