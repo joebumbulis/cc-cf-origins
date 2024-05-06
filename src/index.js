7831fac675fe40b3744108884b9a874c42a881e3
Joe Bumbulis <joebumbulis@gmail.com>
2024-05-06 13:59:57 -0500

    updates wrangler and index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 // src/index.js
 var src_default = {
 	async fetch(request) {
-		const isGetRequest = (request) => request.method === "GET";
-		const max = 4; 
-		const tableCodes = {
-			0: "1",
-			1: "2",
-			2: "3",
-			3: "4"
-		};
+		const remote = "https://example.com";
 
-		const generateRandomNumber = (tableCodes, max) => {
-			const randomNumber = Math.floor(Math.random() * max);
-			return tableCodes[randomNumber];
-		};
-
-		const response = isGetRequest ? `Random Number Generated: ${generateRandomNumber(tableCodes, max)}` : "please use GET";
-
-		return new Response(response);
+		return await this.fetch(remote, request);
 	}
 };
 export {
