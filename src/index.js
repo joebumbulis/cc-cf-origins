d75126899c6d57953c4f7acf015799c149380556
Joe Bumbulis <joebumbulis@gmail.com>
2024-05-10 15:01:40 -0500

    adds custom domain to wrangler

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 // src/index.js
 var src_default = {
 	async fetch(request, env, context) {
-		const url = new URL(request.url);
+		if (request.method === "GET") {
+			 const result = await fetch(request);
 
-		const someCustomKey = `https://${url.hostname}${url.pathname}`;
-
-		let response = await fetch(request, {
-			cf: {
-				cacheTtl: 5,
-				cacheEverything: true,
-				cacheKey: someCustomKey,
-			},
-		})
-
-		response = new Response(response.body, response)
-
-		response.headers.set('Cache-Control', 'max-age=1500')
-
-		context.waitUntil(caches.default.put(request, response.clone()))
-		console.log("response", response);
-		return response
+			 
+			 console.log('result cf object', result.cf)
+			 return result
+		} else {
+			console.log('non-GET')
+		}
 	}
 };
 export {
